12d1c24497a27e26c5575387b9cd43141368a220
Doren Lan <dlan@hmc.edu>
2016-09-07 21:28:21 -0700

    tutorial

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Doren Lan on 9/6/16.
+//  Copyright © 2016 Doren Lan. All rights reserved.
+//
+
+import UIKit
+class AboutViewController: UIViewController {
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
